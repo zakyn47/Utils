f5cdd54dc9a140ecadbf547c2c041536544282fe
zakyn47 <zalcik.lukas@seznam.cz>
2022-11-03 22:34:04 +0100

    regex test

--- UrlGrabber.py
+++ UrlGrabber.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException, NoSuchElementException
 from locators import Locators
 import time
 import re
@@ -28,8 +29,19 @@ class UrlGrabber:
                 if link.get_attribute("href").startswith("http"):
                     self.urls.append(link.get_attribute("href"))
                     print(link.get_attribute("href"))
-        except AttributeError:
+                    with open("links.txt", "a") as f:
+                        f.write(link.get_attribute("href"))
+                        f.write("\n")
+        except:
             pass
+
+
+    def mail_regex(self):
+        return re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
+
+
+    def cz_phone_regex(self):
+        return re.compile(r"(\+420)? ?[1-9][0-9]{2} ?[0-9]{3} ?[0-9]{3}")
 
 
     def go_to_next_url(self):
@@ -43,6 +55,12 @@ class UrlGrabber:
         
 
 if __name__ == "__main__":
-    UrlGrabber = UrlGrabber("https://www.reddit.com")
-    UrlGrabber.get_links()
+    UrlGrabber = UrlGrabber("https://www.reddit.com/")
+    start_time = time.time()
 
+    UrlGrabber.get_links()
+    UrlGrabber.go_to_next_url()
+
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time}")
+    print(UrlGrabber.get_email_regex())
